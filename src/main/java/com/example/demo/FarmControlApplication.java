2d29287fa0384163f7a168e0750399e57dfdebe2
JesusCiber <fjesuscl@gmail.com>
2025-05-07 10:27:04 +0200

    init commit
    # Conflicts:
    #	src/main/java/com/example/demo/FarmControlApplication.java
    #	src/test/java/com/example/demo/FarmControlApplicationTests.java

---
+++ src/main/java/com/example/demo/FarmControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmControlApplication.class, args);
+	}
+
+}
