51366ac33dbd3acae662efb596f19c3df7684666
JesusCiber <fjesuscl@gmail.com>
2025-05-07 16:54:47 +0200

    Pruebas en postman

--- src/main/java/controllers/FarmController.java
+++ src/main/java/controllers/FarmController.java
@@ -2,24 +2,28 @@ package controllers;
 
 import jakarta.persistence.EntityNotFoundException;
 import models.Farm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import repositories.FarmRepository;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/farms")
+@RequestMapping("/farm")
 public class FarmController {
 
+    @Autowired
     private FarmRepository farmRepository;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Farm> getAllFarms() {
         return farmRepository.findAll();
     }
+
+
 
     @GetMapping("/{id}")
     public ResponseEntity<Farm> getFarmById(@PathVariable Long id){
@@ -33,5 +37,25 @@ public class FarmController {
     public Farm createFarm(@RequestBody Farm farm){
         return farmRepository.save(farm);
 
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Farm> updateFarm(@PathVariable Long id, @RequestBody Farm updatedFarm){
+        Farm farm = farmRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Farm with ID " + id + " not found"));
+        farm.setName(updatedFarm.getName());
+        farm.setLocation(updatedFarm.getLocation());
+        farm.setOwner(updatedFarm.getOwner());
+
+        return ResponseEntity.ok(farmRepository.save(farm));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFarm(@PathVariable Long id){
+        Farm farm = farmRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Farm with ID " + id + " not found"));
+        farmRepository.delete(farm);
+
+        return ResponseEntity.noContent().build();
     }
 }
